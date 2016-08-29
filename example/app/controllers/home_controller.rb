f2632e58e42e63a0bc376e26baa18c7c8f1c7f04
shawn <shawn@naturalprofessional.com>
2016-08-29 16:59:46 -0700

    Add devise to test rails  app

---
+++ example/app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  before_action :authenticate_monster!
+
+def index
+end
+end
---
+++ example/app/models/monster.rb
@@ -0,0 +1,6 @@
+class Monster < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
